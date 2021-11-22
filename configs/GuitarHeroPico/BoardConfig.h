8f6a0ee348930ba8b4d0f8f2f4422223c8f1dcd6
Blitz_TCX <austin_schwartz02@yahoo.com>
2021-11-21 18:07:58 -0600

    commit on 'main'

--- configs/GuitarHeroPico/BoardConfig.h
+++ configs/GuitarHeroPico/BoardConfig.h
@@ -2,9 +2,13 @@
 
 #define PIN_DPAD_UP     13
 #define PIN_DPAD_DOWN   12
+#define GAMEPAD_MASK_LEFT
+#define GAMEPAD_MASK_RIGHT
 #define PIN_BUTTON_B1   0
 #define PIN_BUTTON_B2   1
 #define PIN_BUTTON_B3   2
 #define PIN_BUTTON_B4   3
 #define PIN_BUTTON_L1   4
 #define PIN_BUTTON_R2   26
+#define PIN_BUTTON_S1   11
+#define PIN_BUTTON_S2   10
